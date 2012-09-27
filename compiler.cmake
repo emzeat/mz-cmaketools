9fb14f06eee0f6427a74766d36c3a4e4722b8431
marius <marius@9e4c620c-c391-4b63-a7fb-4924991ea895>
2012-09-27 16:12:57 +0000

    Add proper license headers
    git-svn-id: http://opensource.mlba-team.de/svn/common/CMake@450 9e4c620c-c391-4b63-a7fb-4924991ea895

--- compiler.cmake
+++ compiler.cmake
@@ -1,3 +1,26 @@
+##
+# Copyright (c) 2008-2012 Marius Zwicker
+# All rights reserved.
+# 
+# @LICENSE_HEADER_START:Apache@
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
+# http://www.mlba-team.de
+# 
+# @LICENSE_HEADER_END:Apache@
+##
+
 ##################################################
 #
 #	BUILD/COMPILER.CMAKE
@@ -8,7 +31,6 @@
 # 	compiler variables. If available features
 #	for c++0x will be enabled automatically
 #
-#	(c) 2010-2012 Marius Zwicker
 #
 #
 # PROVIDED CMAKE VARIABLES
