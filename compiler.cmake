163f4232525e847fbb1e71c2861f286713b7d66b
marius <marius@9e4c620c-c391-4b63-a7fb-4924991ea895>
2012-10-05 06:54:37 +0000

    Fix the detection of the clang compiler version, fix some identation
    git-svn-id: http://opensource.mlba-team.de/svn/common/CMake@457 9e4c620c-c391-4b63-a7fb-4924991ea895

--- compiler.cmake
+++ compiler.cmake
@@ -208,7 +208,7 @@ function(__MZ_COMPILER_IS_CLANG _OUTPUT _OUTPUT_VERSION)
   endif()
 
   string(
-    REGEX REPLACE "clang version ([0-9])\\.([0-9])(\\.[0-9])? \(.+\)" "\\1\\2"
+    REGEX REPLACE "(.*) version ([0-9])\\.([0-9])(\\.[0-9])?(\\-[0-9])?(~mlba)? \(.+\)" "\\2\\3"
     _MZ_CLANG_VERSION ${_MZ_CLANG_VERSION}
   )
   
@@ -242,17 +242,17 @@ if(NOT MZ_COMPILER_TEST_HAS_RUN)
 		set(WINDOWS TRUE CACHE INTERNAL WINDOWS )
 	endif()
 	
-    # clang is gcc compatible but still different
-    __MZ_COMPILER_IS_CLANG( _MZ_TEST_CLANG COMPILER_VERSION )
-    if( _MZ_TEST_CLANG )
-        	mz_message("compiler is clang")
-        set(MZ_IS_CLANG TRUE CACHE INTERNAL MZ_IS_CLANG)
-    endif()	
+  # clang is gcc compatible but still different
+  __MZ_COMPILER_IS_CLANG( _MZ_TEST_CLANG COMPILER_VERSION )
+  if( _MZ_TEST_CLANG )
+    	mz_message("compiler is clang")
+      set(MZ_IS_CLANG TRUE CACHE INTERNAL MZ_IS_CLANG)
+  endif()	
 	
 	# gnu compiler
 	#message("IS_GCC ${CMAKE_COMPILER_IS_GNU_CC}")
 	if(UNIX OR MINGW)
-        mz_message("GCC compatible compiler found")
+    mz_message("GCC compatible compiler found")
 		
 		set(MZ_IS_GCC TRUE CACHE INTERNAL MZ_IS_GCC)
 		
@@ -268,17 +268,17 @@ if(NOT MZ_COMPILER_TEST_HAS_RUN)
 				__Boost_MZ_COMPILER_DUMPVERSION(COMPILER_VERSION)
 		endif()
 		mz_message("compiler version ${COMPILER_VERSION}")
-		if(COMPILER_VERSION STRGREATER "45")
+		if(NOT MZ_IS_CLANG AND COMPILER_VERSION STRGREATER "45")
 			mz_message("C++11 support detected")
 			set(MZ_HAS_CXX0X TRUE CACHE INTERNAL MZ_HAS_CXX0X)
 			set(MZ_HAS_CXX11 TRUE CACHE INTERNAL MZ_HAS_CXX11)
-		elseif(COMPILER_VERSION STRGREATER "44")
+		elseif(NOT MZ_IS_CLANG AND COMPILER_VERSION STRGREATER "44")
 			mz_message("experimental C++0x support detected")
 			set(MZ_HAS_EXPERIMENTAL_CXX0X TRUE CACHE BOOL MZ_HAS_EXPERIMENTAL_CXX0X)
 			set(MZ_HAS_CXX0X TRUE CACHE BOOL MZ_HAS_CXX0X)
 			set(MZ_HAS_CXX11 TRUE CACHE BOOL MZ_HAS_CXX11)
 		elseif(MZ_IS_CLANG AND COMPILER_VERSION STRGREATER "29")
-			mz_message("C++11 support detected")
+			mz_message("clang 3.1+ with C++11 support detected")
 			set(MZ_HAS_CXX0X TRUE CACHE INTERNAL MZ_HAS_CXX0X)
 			set(MZ_HAS_CXX11 TRUE CACHE INTERNAL MZ_HAS_CXX11)		
 		endif()
@@ -313,26 +313,26 @@ if(NOT MZ_COMPILER_TEST_HAS_RUN)
 		set(MZ_32BIT ON CACHE INTERNAL MZ_32BIT)
 	endif()
 
-        # configured build type
-        # NOTE: This can be overriden e.g. on Visual Studio
-        if(CMAKE_BUILD_TYPE STREQUAL "Release")
-            set(MZ_IS_RELEASE TRUE CACHE INTERNAL MZ_IS_RELEASE)
-            mz_debug_message("CMake run in release mode")
-        endif()
+  # configured build type
+  # NOTE: This can be overriden e.g. on Visual Studio
+  if(CMAKE_BUILD_TYPE STREQUAL "Release")
+    set(MZ_IS_RELEASE TRUE CACHE INTERNAL MZ_IS_RELEASE)
+    mz_debug_message("CMake run in release mode")
+  endif()
 
 endif() #MZ_COMPILER_TEST_HAS_RUN
 
 # determine current date and time
 if(WINDOWS)
-    execute_process(COMMAND "date" "/T" OUTPUT_VARIABLE MZ_DATE_STRING)
+  execute_process(COMMAND "date" "/T" OUTPUT_VARIABLE MZ_DATE_STRING)
 else() # Sun, 11 Dec 2011 12:07:00 +0200
-    execute_process(COMMAND "date" "+%a, %d %b %Y %T %z" OUTPUT_VARIABLE MZ_DATE_STRING)
+  execute_process(COMMAND "date" "+%a, %d %b %Y %T %z" OUTPUT_VARIABLE MZ_DATE_STRING)
 endif()
 mz_message("Today is: ${MZ_DATE_STRING}")
 
 # optional C++0x/c++11 features on gcc (on vs2010 this is enabled by default)
 if(MZ_IS_GCC AND MZ_HAS_CXX0X) # AND NOT DARWIN)
-    mz_add_cxx_flag(GCC -std=gnu++0x)
+  mz_add_cxx_flag(GCC -std=gnu++0x)
 	mz_message("forcing C++11 support on this platform")
 endif()
 
@@ -340,11 +340,11 @@ endif()
 mz_add_definition(${CMAKE_SYSTEM_PROCESSOR}=1)
 mz_add_flag(GCC -Wall -Werror -Wno-unused-function)
 if(WINDOWS)
-    mz_add_definition(WIN32=1 WINDOWS=1)
+  mz_add_definition(WIN32=1 WINDOWS=1)
 elseif(IOS)
 	mz_add_definition(IOS=1)
 else()
-    mz_add_definition(${CMAKE_SYSTEM_NAME}=1)
+  mz_add_definition(${CMAKE_SYSTEM_NAME}=1)
 endif()
 
 
@@ -355,11 +355,11 @@ if(MZ_IS_GCC)
 	SET(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O3")
 		
 	if(WINDOWS)
-        if(MZ_64BIT)
-        	mz_add_definition("WIN32_MINGW64=1")
-    	else()
-    	    mz_add_definition("WIN32_MINGW=1")
-    	endif()
+    if(MZ_64BIT)
+      mz_add_definition("WIN32_MINGW64=1")
+    else()
+      mz_add_definition("WIN32_MINGW=1")
+    endif()
 	endif()
 
 elseif(MZ_IS_VS)
@@ -370,22 +370,22 @@ elseif(MZ_IS_VS)
 endif()
 
 if(MZ_HAS_CXX11)
-    mz_add_definition(MZ_HAS_CXX11=1)
-    mz_add_definition(MZ_HAS_CXX0X=1)
+  mz_add_definition(MZ_HAS_CXX11=1)
+  mz_add_definition(MZ_HAS_CXX0X=1)
 endif()
 
 # On Windows, MS Visual Studio will ignore the
 # environment variables INCLUDE and LIB by default. This forces
 # them to be used
 if(WINDOWS)
-    FOREACH(_inc $ENV{INCLUDE})
-		file(TO_CMAKE_PATH ${_inc} _inc)
-        include_directories("${_inc}")
-        mz_debug_message("Including ${_inc} for headers")
-    ENDFOREACH(_inc)
-    FOREACH(_lib $ENV{LIB})
-		file(TO_CMAKE_PATH ${_lib} _lib)
-        link_directories("${_lib}")
-        mz_debug_message("Including ${_lib} for libs")
-    ENDFOREACH(_lib)	
+  foreach(_inc $ENV{INCLUDE})
+    file(TO_CMAKE_PATH ${_inc} _inc)
+    include_directories("${_inc}")
+    mz_debug_message("Including ${_inc} for headers")
+  endforeach(_inc)
+  foreach(_lib $ENV{LIB})
+    file(TO_CMAKE_PATH ${_lib} _lib)
+    link_directories("${_lib}")
+    mz_debug_message("Including ${_lib} for libs")
+  endforeach(_lib)	
 endif()
