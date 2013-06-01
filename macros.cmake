81a7fb6f57c86e066aba4202e68ecec64118e461
marius <marius@9e4c620c-c391-4b63-a7fb-4924991ea895>
2013-06-01 08:52:01 +0000

    New macro mz_find_checkout_library and various other minor improvements
    git-svn-id: http://opensource.mlba-team.de/svn/common/CMake@583 9e4c620c-c391-4b63-a7fb-4924991ea895

--- macros.cmake
+++ macros.cmake
@@ -44,12 +44,29 @@
 # mz_find_include_library <name>  SYS <version> SRC <directory> <include_dir> <target>
 #       useful when providing a version of a library within the
 #       own sourcetree but prefer the system's library version over it.
-#       Will search for the given header in the system includes and when
+#       Will search for the given library in the system using find_package and when
 #       not found, it will include the given directory which should contain
 #       a cmake file defining the given target.
 #       After calling this macro the following variables will be declared:
 #           <name>_INCLUDE_DIRS The directory containing the header or 
 #                              the passed include_dir if the lib was not 
+#                              found on the system
+#           <name>_LIBRARIES The libs to link against - either lib or target
+#           <name>_FOUND true if the lib was found on the system
+#           <name>_IGNORE_SYSTEM_LIBRARY Will be an option available
+#                              for custom configuration. Enable to ignore the
+#                              detected system install library and force use
+#                              of the version in <directory>
+#
+# mz_find_checkout_library <name>  SYS <version> SVN <repository> <target_dir> <include_dir> <target>
+#       useful when needing a version of a library but not sure wether
+#       it is available on the target system.
+#       Will search for the given library in the system using find_package and when
+#       not found, it will do a checkout of the given repository which should contain
+#       a cmake file defining the given target.
+#       After calling this macro the following variables will be declared:
+#           <name>_INCLUDE_DIRS The directory containing the header or
+#                              the passed include_dir if the lib was not
 #                              found on the system
 #           <name>_LIBRARIES The libs to link against - either lib or target
 #           <name>_FOUND true if the lib was found on the system
@@ -168,9 +185,15 @@ endmacro()
 macro(mz_find_include_library _NAME SYS _VERSION SRC _DIRECTORY _INC_DIR _TARGET)
     
     STRING(TOUPPER ${_NAME} _NAME_UPPER)
-    STRING(REPLACE "-" "_" _NAME_UPPER "${_NAME_UPPER}")
-    
+    STRING(REPLACE "-" "_" _NAME_UPPER "${_NAME_UPPER2}") # special care for libraries with - in their names
+    get_filename_component(_DIRECTORY_ABS ${_DIRECTORY_DIR} ABSOLUTE)
+
     find_package( ${_NAME} ${_VERSION} )
+
+    if( ${_NAME}_FOUND OR ${_NAME_UPPER2}_FOUND )
+        set( ${_NAME_UPPER}_FOUND TRUE )
+    endif()
+
     if( ${_NAME_UPPER}_FOUND )
         option(${_NAME_UPPER}_IGNORE_SYSTEM_LIBRARY "Force use of the in-source library version of ${_NAME}" OFF)
     else()
@@ -180,8 +203,66 @@ macro(mz_find_include_library _NAME SYS _VERSION SRC _DIRECTORY _INC_DIR _TARGET
     if( NOT ${_NAME_UPPER}_FOUND OR ${_NAME_UPPER}_IGNORE_SYSTEM_LIBRARY )
         set(${_NAME_UPPER}_INCLUDE_DIRS ${_INC_DIR})
         set(${_NAME_UPPER}_LIBRARIES ${_TARGET} ${ARGN})
+        set(${_NAME_UPPER}_FOUND TRUE)
         
-        mz_add_library(${_NAME} ${_DIRECTORY})    
+        mz_message("No system library for '{_NAME}', building own version")
+        if( NOT ${_TARGET} STREQUAL "" )
+            mz_add_library(${_NAME} ${_DIRECTORY})
+        else()
+            mz_debug_message("${_NAME} is a header only library, no new target")
+        endif()
+    endif()
+
+endmacro()
+
+macro(mz_find_checkout_library _NAME SYS _VERSION SVN _REPOSITORY _DEST_DIR _INC_DIR _TARGET)
+
+    STRING(TOUPPER ${_NAME} _NAME_UPPER2)
+    STRING(REPLACE "-" "_" _NAME_UPPER "${_NAME_UPPER2}") # special care for libraries with - in their names
+    get_filename_component(_DEST_DIR_ABS ${_DEST_DIR} ABSOLUTE)
+
+    find_package( ${_NAME} ${_VERSION} )
+
+    if( ${_NAME}_FOUND OR ${_NAME_UPPER2}_FOUND )
+        set( ${_NAME_UPPER}_FOUND TRUE )
+    endif()
+
+    if( ${_NAME_UPPER}_FOUND )
+        option(${_NAME_UPPER}_IGNORE_SYSTEM_LIBRARY "Force use of the in-source library version of ${_NAME}" OFF)
+    else()
+        option(${_NAME_UPPER}_IGNORE_SYSTEM_LIBRARY "Force use of the in-source library version of ${_NAME}" ON)
+    endif()
+
+    if( NOT ${_NAME_UPPER}_FOUND OR ${_NAME_UPPER}_IGNORE_SYSTEM_LIBRARY )
+        mz_message("No system library for '${_NAME}', retrieving own version")
+
+        find_package( Subversion )
+        if( SUBVERSION_FOUND )
+            if(NOT EXISTS ${_DEST_DIR_ABS})
+                execute_process(
+                    COMMAND ${Subversion_SVN_EXECUTABLE} checkout ${_REPOSITORY} ${_DEST_DIR_ABS}
+                    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                    OUTPUT_VARIABLE _SVN_OUT
+                )
+                mz_debug_message( ${_SVN_OUT} )
+            endif()
+        else()
+            mz_message( "WARNING: No Subversion found, please do a manual checkout of ${_REPOSITORY} to ${_DEST_DIR_ABS} and re-run project generation" )
+        endif()
+
+        if(NOT EXISTS ${_DEST_DIR_ABS})
+            mz_error_message( "Failed to include ${_DEST_DIR_ABS} as library source, please make sure a valid checkout exists there" )
+        endif()
+
+        set(${_NAME_UPPER}_INCLUDE_DIRS ${_INC_DIR})
+        set(${_NAME_UPPER}_LIBRARIES ${_TARGET} ${ARGN})
+        set(${_NAME_UPPER}_FOUND TRUE)
+
+        if( NOT ${_TARGET} STREQUAL "" )
+            mz_add_library(${_NAME} ${_DEST_DIR})
+        else()
+            mz_debug_message("${_NAME} is a header only library, no new target")
+        endif()
     endif()
 
 endmacro()
