97cf37f2da8f9b1a70291caccb0f5439bb8ade4e
marius <marius@9e4c620c-c391-4b63-a7fb-4924991ea895>
2013-06-03 07:47:52 +0000

    Use std=c+11 instead of gnu++0
    git-svn-id: http://opensource.mlba-team.de/svn/common/CMake@588 9e4c620c-c391-4b63-a7fb-4924991ea895

--- compiler.cmake
+++ compiler.cmake
@@ -363,7 +363,7 @@ mz_message("Today is: ${MZ_DATE_STRING}")
 
 # optional C++0x/c++11 features on gcc (on vs2010 this is enabled by default)
 if(MZ_IS_GCC AND MZ_HAS_CXX0X) # AND NOT DARWIN)
-    mz_add_cxx_flag(GCC -std=gnu++0x)
+    mz_add_cxx_flag(GCC -std=c++11)
     mz_message("forcing C++11 support on this platform")
 endif()
 
