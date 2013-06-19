f21d80919fd52bcd86751c6fb7c1c2e12a1a4eec
marius <marius@9e4c620c-c391-4b63-a7fb-4924991ea895>
2013-06-19 07:12:28 +0000

    Worked around an issue with gcc > 4.7 and eigen3 (Wenum-compare)
    git-svn-id: http://opensource.mlba-team.de/svn/common/CMake@602 9e4c620c-c391-4b63-a7fb-4924991ea895

--- compiler.cmake
+++ compiler.cmake
@@ -367,8 +367,13 @@ mz_message("User is: ${MZ_USER_STRING}")
 
 # optional C++0x/c++11 features on gcc (on vs2010 this is enabled by default)
 if(MZ_IS_GCC AND MZ_HAS_CXX0X) # AND NOT DARWIN)
-    mz_add_cxx_flag(GCC -std=gnu++11)
+    mz_add_cxx_flag(GCC -std=c++11 -fgnu-keywords -Wno-unused-local-typedefs)
     mz_message("forcing C++11 support on this platform")
+endif()
+
+# work around an issue with gcc > 4.7 and eigen3
+if(COMPILER_VERSION STRGREATER "47")
+    mz_add_cxx_flag(GCC -Wno-enum-compare)
 endif()
 
 # compiler flags
