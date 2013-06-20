5eb35d84e56c8a11c194eb66a5d6d5039fc69593
marius <marius@9e4c620c-c391-4b63-a7fb-4924991ea895>
2013-06-20 10:22:31 +0000

    Further improvements to the autoformat targets and change detection
    git-svn-id: http://opensource.mlba-team.de/svn/common/CMake@606 9e4c620c-c391-4b63-a7fb-4924991ea895

--- autoformat.cmake
+++ autoformat.cmake
@@ -32,8 +32,13 @@ if(NOT WINDOWS)
   set(MZ_CPPLINT_BIN ${MZ_TOOLS_PATH}/cpplint.py CACHE PATH "Path to cpplint" FORCE)
 endif()
 
-option(MZ_DO_AUTO_FORMAT "Enable to run autoformat on configured targets" ON)
-option(MZ_DO_CPPLINT "Enable to run cpplint on configured targets" ON)
+if( MZ_IS_RELEASE )
+    option(MZ_DO_AUTO_FORMAT "Enable to run autoformat on configured targets" OFF)
+    option(MZ_DO_CPPLINT "Enable to run cpplint on configured targets" OFF)
+else()
+    option(MZ_DO_AUTO_FORMAT "Enable to run autoformat on configured targets" ON)
+    option(MZ_DO_CPPLINT "Enable to run cpplint on configured targets" ON)
+endif()
 
 macro(mz_auto_format _TARGET)
   set(_sources ${ARGN})
@@ -62,23 +67,23 @@ macro(mz_auto_format _TARGET)
   set(_new_target_lint ${_TARGET}_cpplint)
 
   if( MZ_UNCRUSTIFY_BIN AND MZ_DO_AUTO_FORMAT )
-    configure_file(${MZ_TOOLS_PATH}/autoformat.c.in ${CMAKE_CURRENT_BINARY_DIR}/format_step.c)
     add_library(${_new_target_format} STATIC ${CMAKE_CURRENT_BINARY_DIR}/format_step.c)
     set_target_properties(${_new_target_format} PROPERTIES ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
     add_custom_command(
         OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/format_step.c
         COMMAND ${MZ_UNCRUSTIFY_BIN} -c ${CMAKE_BINARY_DIR}/autoformat.cfg --no-backup --mtime ${_sources2}
+        COMMAND ${CMAKE_COMMAND} -E copy ${MZ_TOOLS_PATH}/autoformat.c.in ${CMAKE_CURRENT_BINARY_DIR}/format_step.c
         DEPENDS ${_sources}
         WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
     )
   endif()
   if( MZ_CPPLINT_BIN AND MZ_DO_CPPLINT )
-    configure_file(${MZ_TOOLS_PATH}/autoformat.c.in ${CMAKE_CURRENT_BINARY_DIR}/lint_step.c)
     add_library(${_new_target_lint} STATIC ${CMAKE_CURRENT_BINARY_DIR}/lint_step.c)
     set_target_properties(${_new_target_lint} PROPERTIES ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
     add_custom_command(
         OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/lint_step.c
         COMMAND ${MZ_CPPLINT_BIN} --root=${CMAKE_CURRENT_LIST_DIR} --filter=${CPPLINT_FILTERS} ${_sources2}
+        COMMAND ${CMAKE_COMMAND} -E copy ${MZ_TOOLS_PATH}/autoformat.c.in ${CMAKE_CURRENT_BINARY_DIR}/lint_step.c
         DEPENDS ${_sources}
         WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
     )
