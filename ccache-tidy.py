emzeat/mz-cmaketools

commit 2b8cfeb425f663b17750332182a014cf65bcc406

linting: Fix invocation with .py not associated to python3
ccache-tidy.py would not reliably run on e.g. Windows when .py was not associated
with python3 for execution but some other interpreter.

ccache-tidy.py
@@ -95,7 +95,7 @@ def invoke_clang_tidy(with_args, capture_output=False) -> int:
 
 def invoke_ccache(with_args, with_env) -> int:
     '''Invokes ccache using the given arguments and env'''
-    with_args = [CCACHE_TIDY_SELF] + with_args
+    with_args = [sys.executable, CCACHE_TIDY_SELF] + with_args
     log_debug(f"Invoking {CCACHE} with args={with_args} env={with_env}")
     try:
         patched_env = os.environ.copy()

linting.cmake
@@ -99,7 +99,7 @@ if( CLANG_TIDY )
     mz_message("Linting (C++) will be accelerated using ccache")
     set(MZ_CLANG_TIDY
       ${CMAKE_COMMAND} -E env CLANG_TIDY=${CLANG_TIDY} CCACHE=${CCACHE}
-      ${CMAKE_SOURCE_DIR}/build/ccache-tidy.py
+      ${PYTHON3} ${CMAKE_SOURCE_DIR}/build/ccache-tidy.py
     )
   else()
     set(MZ_CLANG_TIDY

commit 5894ce4efa884b773c09f72a7ba1e97d7c2c270c

linting: Fix regression introduced by 2b8cfeb
When forcing ccache-tidy.py to go through python3 also on Windows we accidentially
broke the ccache acceleration because ccache was not able to parse the commandline
anymore. This is because it expects a single compiler executable as first argument only.

As an alternative solution there is CCACHE_PREFIX which we now use to pass the
python3 executable needed to run the python file.

ccache-tidy.py
@@ -95,11 +95,12 @@ def invoke_clang_tidy(with_args, capture_output=False) -> int:
 
 def invoke_ccache(with_args, with_env) -> int:
     '''Invokes ccache using the given arguments and env'''
-    with_args = [sys.executable, CCACHE_TIDY_SELF] + with_args
+    with_args = [CCACHE_TIDY_SELF] + with_args
     log_debug(f"Invoking {CCACHE} with args={with_args} env={with_env}")
     try:
         patched_env = os.environ.copy()
         patched_env.update(with_env)
+        patched_env['CCACHE_PREFIX'] = sys.executable
         subprocess.check_call([CCACHE] + with_args, env=patched_env)
         return 0
     except subprocess.CalledProcessError as error: